66059908e5970368972c5f6f35cab73d31a011f9
David Trimmer <dbtrimmer1@gmail.com>
2020-11-07 01:53:11 -0500

    wired up the donut multiplier button

--- src/app.js
+++ src/app.js
@@ -2,31 +2,33 @@ import { DonutMaker } from "/src/js/DonutMaker.js";
 
 const donutMaker = new DonutMaker();
 
-const updateDonutCount = function(){
+const updateDonutCount = function () {
     const donuts = document.querySelector('.donut_count');
-    donuts.innerText = Math.round(donutMaker.donutCount * 100)/100;
+    donuts.innerText = Math.round(donutMaker.donutCount * 100) / 100;
     checkAutoClickerButton();
+    checkDonutMultiplierButton();
 }
 //donut Maker
 const clicks = document.querySelector('.add__donut');
-clicks.addEventListener('click', ()=> {
+clicks.addEventListener('click', () => {
     donutMaker.recordClick();
     updateDonutCount();
-    
+
 })
 
 //autoclickers
-const updateAutoClickerCount = function(){
+const updateAutoClickerCount = function () {
     const autoClickerCount = document.querySelector('.auto_clicker_count');
     autoClickerCount.innerText = donutMaker.autoClickerCount;
 }
 
 const autoClickers = document.querySelector('.purchase__auto_clickers');
-autoClickers.addEventListener('click', ()=> {
+autoClickers.addEventListener('click', () => {
     donutMaker.addAutoClicker();
     updateAutoClickerCount();
     updateDonutCount();
     checkAutoClickerButton();
+    checkDonutMultiplierButton();
 })
 
 function checkAutoClickerButton() {
@@ -40,12 +42,24 @@ function checkAutoClickerButton() {
 
 
 //donut multipliers
-const updateMultiplierCount = function() {
-    const multiplierCount = document.querySelector('donut_multiplier_count');
+const updateMultiplierCount = function () {
+    const multiplierCount = document.querySelector('.donut_multiplier_count');
     multiplierCount.innerText = donutMaker.donutMultiplier;
 }
 
-
-
-
+const donutMultiplier = document.querySelector('.purchase_donut_mulitipliers');
+donutMultiplier.addEventListener('click', () => {
+    donutMaker.addDonutMultiplier();
+    updateMultiplierCount();
+    updateDonutCount();
+    checkAutoClickerButton();
+    checkDonutMultiplierButton();
+})
+function checkDonutMultiplierButton() {
+    if (donutMaker.donutCount >= donutMaker.donutMultiplierCost) {
+        donutMultiplier.disabled = false;
+    } else {
+        donutMultiplier.disabled = true;
+    }
+}
 
